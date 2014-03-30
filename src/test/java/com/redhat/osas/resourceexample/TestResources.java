21b5d0d234825bd7b61624979e004d7418460702
jottinger <joeo@enigmastation.com>
2014-03-30 09:06:14 -0400

    Add properties example

--- src/test/java/com/redhat/osas/resourceexample/TestResources.java
+++ src/test/java/com/redhat/osas/resourceexample/TestResources.java
@@ -2,10 +2,13 @@ package com.redhat.osas.resourceexample;
 
 import org.testng.annotations.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Properties;
 import java.util.Scanner;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 
 /**
  * User: jottinge
@@ -16,6 +19,7 @@ public class TestResources {
     @Test
     public void readRelative() {
         InputStream is = this.getClass().getResourceAsStream("packageexample.txt");
+        assertNotNull(is);
         Scanner scanner = new Scanner(is);
         String input = scanner.next();
         assertEquals(input, "local");
@@ -24,9 +28,22 @@ public class TestResources {
     @Test
     public void readAbsolute() {
         InputStream is = this.getClass().getResourceAsStream("/absoluteexample.txt");
+        assertNotNull(is);
         Scanner scanner = new Scanner(is);
         String input = scanner.next();
         assertEquals(input, "absolute");
     }
+
+    @Test
+    public void readProperty() throws IOException {
+        InputStream is = this.getClass().getResourceAsStream("/configexample.properties");
+        assertNotNull(is);
+Properties properties=new Properties();
+        properties.load(is);
+        assertEquals(properties.getProperty("foo"), "bar");
+        assertEquals(properties.getProperty("baz"), "bletch");
+        assertEquals(properties.getProperty("quux"), "snafu");
+    }
+
 
 }
